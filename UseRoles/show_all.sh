e03237498d3e00d655d9522008f3ae5556fae4d3
ne0kk <neok.kem@gmail.com>
2024-12-09 06:54:13 +0700

    08-ansible-04-role

---
+++ UseRoles/show_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run -it --name clickhouse-01 -d ubuntu:20.04
+docker exec clickhouse-01 /bin/bash -c 'apt-get update -y' 
+docker exec clickhouse-01 /bin/bash -c 'apt-get install -y python2.7'
+docker exec clickhouse-01 /bin/bash -c 'apt install openssh-server -y'  
